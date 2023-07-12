ef5e1c2bf0d07a97567781f5c2f9a7338f87efff
pujithaGuttala <108428114+pujithaGuttala@users.noreply.github.com>
2023-07-12 21:38:56 +0530

    Add files via upload

---
+++ colorbutton.js
@@ -0,0 +1,24 @@
+let p = document.getElementsByClassName("p");
+
+let c=document.getElementById("red");
+c.onclick=function(){
+    p[0].style.backgroundColor = "red";
+};
+
+
+let d=document.getElementById("yellow");
+d.onclick=function(){
+    p[0].style.backgroundColor = "yellow";
+};
+
+
+let s=document.getElementById("blue");
+s.onclick=function(){
+    p[0].style.backgroundColor = "blue";
+};
+
+
+let z=document.getElementById("green");
+z.onclick=function(){
+    p[0].style.backgroundColor = "green";
+};
